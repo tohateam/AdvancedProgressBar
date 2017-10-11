469084c1a617437fb04899506cfa4b9a1773c564
tohateam <tohaguk@outlook.com>
2017-10-11 14:02:04 +0300

    Исправлено отображение метки

--- ProgressBarLibrary/ColorProgresBar.Designer.cs
+++ ProgressBarLibrary/ColorProgresBar.Designer.cs
@@ -28,14 +28,15 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(54, 5);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.Size = new System.Drawing.Size(150, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "0 %";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ColorProgresBar
             // 
@@ -46,7 +47,6 @@
             this.Name = "ColorProgresBar";
             this.Size = new System.Drawing.Size(150, 23);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
